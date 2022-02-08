c9199dbaa33c9e5b2ae765a2037d307c5e4529bb
Hugo Deniau <hugo.deniau91@gmail.com>
2022-02-08 10:46:11 -0500

    Getters and Setters for Ships

--- eCommerce/Ship.cs
+++ eCommerce/Ship.cs
@@ -4,10 +4,33 @@ class Ship : Container{
     private bool currentAction = null;
     private int targetProduct;
 
+    //A modifier avec le constructeur de Container
     public Ship(int[] itin, int pos, bool currentAct, int targetProd){
         itinéraire = itin;
         position = pos;
         currentAction = currentAct;
         targetProduct = targetProd;
     }
+    
+    public int[] Itineraire{
+        get { return itinéraire;}
+        set { itinéraire = value;}
+    }
+
+    public int Position{
+        get { return position;}
+        set { position = value;}
+    }
+
+    public bool CurrentAction{
+        get { return currentAction;}
+        set { currentAction = value;}
+    }
+
+    public int TargetProduct{
+        get { return targetProduct;}
+        set { targetProduct = value;}
+    }
+
+
 }
