29e33735805fa56104fff67fad5cd0bd6c4076a2
Reynaud <yann.reynaud@isen-ouest.yncrea.fr>
2022-02-12 12:37:45 -0500

    classe trader, lien avec les goods  à vérifier (pas sur de la syntaxe)

---
+++ eCommerce/Trader.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Trader{
+    internal searchForMax(Ship ship, Planet planet){
+        int shipCapacity=ship.myMaxGoods[ship.TargetProduct()]-ship.goods[ship.TargetProduct()];
+        if (ship.ShipAction==buyGoods){  //vaisseau non chargé
+            if(planet.myMaxGoods[ship.TargetProduct()]!=0){
+                if(shipCapacity>=planet.goods[ship.TargetProduct()]){
+                    return planet.goods[ship.TargetProduct()];
+                }
+                else{
+                    return shipCapacity;
+                }
+            }
+            else{
+                return 0;
+            }
+        }
+        else if(ship.ShipAction==sellGoods){ //vaisseau chargé
+            if(planet.myMaxGoods[ship.TargetProduct()]!=0){
+                return ship.goods[ship.TargetProduct()];
+
+            }
+            else{
+                return 0;
+            }
+
+        }
+
+    static void Trade(Planet[] planets){
+        foreach(Planet planet in planets){
+            for(int k=0; k<planet.harbor_nb;k++){
+                if (ship.ShipAction==buyGoods){
+                    ship.AddGoods(ship.TargetProduct(),searchForMax(ship,planet));
+                    planet.RemoveGoods(ship.TargetProduct(),searchForMax(ship,planet));
+                }
+                else if(ship.ShipAction==sellGoods){
+                    planet.AddGoods(ship.TargetProduct(),searchForMax(ship,planet));
+                    ship.RemoveGoods(ship.TargetProduct(),searchForMax(ship,planet));
+
+                }
+            }
+        }
+
+    }
+}
